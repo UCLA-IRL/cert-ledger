6c9613e2a2d3dca9ae15cfb9d233df780c6a4f74
Tianyuan Yu <tianyuan@cs.ucla.edu>
2022-08-15 19:44:49 -0700

    tests: add a test case for dag-edge

--- tests/unit-tests/dag-edge.t.cpp
+++ tests/unit-tests/dag-edge.t.cpp
@@ -83,6 +83,43 @@ BOOST_AUTO_TEST_CASE(Rectangular2)
 {
   /*
    * r1 <------------ r2
+   *  ^                ^
+   *  |                |
+   *  |                |
+   * r3 ------------> r4
+   */
+  Record r1, r2, r3, r4;
+  r1.setName(Name("/r1"));
+  r1.addPointer(r1.getName());
+  
+  r2.setName(Name("/r2"));
+  r2.addPointer(r1.getName());
+   
+  r4.setName(Name("/r4"));
+  r4.addPointer(r2.getName());
+
+  r3.setName(Name("/r3"));
+  r3.addPointer(r1.getName());
+  r3.addPointer(r4.getName());
+
+  EdgeManager eManager;
+  eManager.add(r1);
+  eManager.add(r2);
+  eManager.add(r4);
+  eManager.add(r3);
+
+  BOOST_CHECK_EQUAL(3, eManager.reap(1).size());
+  BOOST_CHECK_EQUAL(2, eManager.reap(2).size());
+  BOOST_CHECK_EQUAL(1, eManager.reap(3).size());
+  for (auto& s : eManager.m_buffer) {
+    std::cerr << s.second;
+  }
+}
+
+BOOST_AUTO_TEST_CASE(Rectangular3)
+{
+  /*
+   * r1 <------------ r2
    *  ^         --->   ^
    *  |      ---       |
    *  |  ----          |
